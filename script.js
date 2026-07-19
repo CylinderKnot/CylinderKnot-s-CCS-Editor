18ae0f9ae5203a297702e36d7bebfd0c98e5783e
CylinderKnot <bb625jhb@gmail.com>
2026-07-18 21:26:17 -0500

    just getting started with main page, added a background image, the script file, and the styles file

---
+++ script.js
@@ -0,0 +1,16 @@
+// bunch of consts up here
+
+const layers = [
+  "tiles",
+  "rapids",
+  "conveyors",
+  "candies/blockers",
+  "encasings",
+  "dispensers",
+  "walls",
+  "crystals",
+  "portals/exits",
+  "selectionImage"
+]
+
+// more stuff here
